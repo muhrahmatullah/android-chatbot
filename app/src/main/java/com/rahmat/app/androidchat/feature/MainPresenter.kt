9977649bc63017c4778f03ee46680f68e972b2f6
Muh. Rahmatullah <muh.rahmatullah@grabtaxi.com>
2020-01-14 22:56:38 +0700

    update coroutine

--- app/src/main/java/com/rahmat/app/androidchat/feature/MainPresenter.kt
+++ app/src/main/java/com/rahmat/app/androidchat/feature/MainPresenter.kt
@@ -6,9 +6,8 @@ import ai.api.model.AIResponse
 import android.util.Log
 import com.google.firebase.database.DatabaseReference
 import com.rahmat.app.androidchat.entity.ChatMessage
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import org.jetbrains.anko.coroutines.experimental.bg
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Job
 
 /**
  * Created by muhrahmatullah on 09/10/18.
@@ -18,26 +17,32 @@ class MainPresenter(val aiDataAIService: AIDataService,
 
     val aiRequest = AIRequest()
 
+    val job = Job()
+
     override fun sendMessage(message: String) {
         val chatMessage = ChatMessage(message, "user")
         ref.child("chat").push().setValue(chatMessage)
         aiRequest.setQuery(message)
 
-        async(UI) {
-            val response = bg {
-                aiDataAIService.request(aiRequest)
-            }
-            if (response.await() != null) {
-                sendMessageToServer(response.await())
-            }else{
-                Log.v("test", "didn't send")
-            }
-        }
+
+
+//        async(UI) {
+//            val response = bg {
+//                aiDataAIService.request(aiRequest)
+//            }
+//            if (response.await() != null) {
+//                sendMessageToServer(response.await())
+//            }else{
+//                Log.v("test", "didn't send")
+//            }
+//        }
     }
-    private fun sendMessageToServer(response: AIResponse?) {
-        val result = response?.result
-        val reply = result?.fulfillment?.speech
-        val chatMessage = ChatMessage(reply, "bot")
-        ref.child("chat").push().setValue(chatMessage)
+    private suspend fun sendMessageToServer(response: AIResponse?) {
+        withContext(Dispatchers.IO) {
+            val result = response?.result
+            val reply = result?.fulfillment?.speech
+            val chatMessage = ChatMessage(reply, "bot")
+            ref.child("chat").push().setValue(chatMessage)
+        }
     }
 }
